a60750a09e6275ecca3d497b8e3f30c00be0e859
Philippe Thevenaz <philippe.thevenaz@icloud.com>
2026-06-12 17:31:35 +0200

    v0.1.1

--- dev/PurePython/dispatch.sh
+++ dev/PurePython/dispatch.sh
@@ -5,5 +5,7 @@ splinekitDir="/Users/pthevena/Documents/Programmation/Python/splinekit"
 cp $devDir/release.yml $splinekitDir/.github/workflows/release.yml
 cp $devDir/pyproject.toml $splinekitDir/pyproject.toml
 cp $devDir/spline_padding.py $splinekitDir/src/splinekit/spline_padding.py
+cp $devDir/test_spline_padding.py $splinekitDir/tests/test_spline_padding.py
+
 
 # Edit splinekit-jupyterlite/environment.yml
